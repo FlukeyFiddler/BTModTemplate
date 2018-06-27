847a11a3f1e52df751ab946db5d220448ccf6b7f
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-27 11:09:08 +0200

    Initial commit

---
+++ BTModTemplate/BTModTemplate.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using nl.flukeyfiddler.bt.BTModTemplate.Utils;
+using nl.flukeyfiddler.bt.Utils.Logger;
+using System.IO;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.bt.BTModTemplate
+{
+    public class BTModTemplate
+    {
+        public static void Init(string modDirectory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("nl.flukeyfiddler.bt.
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+
+            Logger.SetLogFilePath(new LogFilePath(Path.Combine(modDirectory, "Log.txt")));
+            Logger.GameStarted();
+        }
+    }
+}
